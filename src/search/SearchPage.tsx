393ac0b3496dd2dc041d3480d135011b683f398d
Chidi Orji <orjichidi95@gmail.com>
2021-11-09 01:17:32 +0100

    refactor: extract branding
    - make branding reusable

--- src/search/SearchPage.tsx
+++ src/search/SearchPage.tsx
@@ -1,18 +1,12 @@
-import { BsGithub } from 'react-icons/bs';
 
 import styles from './search.module.scss';
 import { SearchForm } from '../shared/SearchForm';
+import { Branding } from '../shared/Branding';
 
 export const SearchPage = (): JSX.Element => {
   return (
     <div className={styles.search_container}>
-      <div className={styles.icon_wrapper}>
-        <div>
-          <BsGithub size={50} />
-        </div>
-        <p>GitHub</p>
-      </div>
-
+      <Branding />
       <SearchForm />
     </div>
   );
