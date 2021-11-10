95099d77a1f2f32a9373d22eefd962a2738fdab9
Chidi Orji <orjichidi95@gmail.com>
2021-11-10 10:28:55 +0100

    test: test login and logout flow

--- src/login/LoginWithGithub.test.tsx
+++ src/login/LoginWithGithub.test.tsx
@@ -1,9 +1,21 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '../test-utils/render';
+import userEvent from '@testing-library/user-event';
+
 import App from '../App';
-import { render, screen } from '../test-utils/render';
 
 describe('Authentication', () => {
-  it('renders login button', () => {
+  window.open = jest.fn();
+
+  it('Should login and logout user', async () => {
     render(<App />);
+    userEvent.click(screen.getByText(/login to github/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/Logging you in./i));
+    expect(screen.getByText(/repository results/)).toBeInTheDocument();
+    userEvent.click(screen.getByText(/logout/i));
     expect(screen.getByText(/login to github/i)).toBeInTheDocument();
   });
 });
