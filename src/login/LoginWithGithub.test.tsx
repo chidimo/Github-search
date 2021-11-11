d22d8d4748f65fb78b0f33c2cff5550714087d03
Chidi Orji <orjichidi95@gmail.com>
2021-11-11 09:40:13 +0100

    test: fix failing authentication flow test

--- src/login/LoginWithGithub.test.tsx
+++ src/login/LoginWithGithub.test.tsx
@@ -14,7 +14,8 @@ describe('Authentication', () => {
     render(<App />);
     userEvent.click(screen.getByText(/login to github/i));
     await waitForElementToBeRemoved(() => screen.getByText(/Logging you in./i));
-    expect(screen.getByText(/repository results/)).toBeInTheDocument();
+    expect(screen.getByText(/search github/i)).toBeInTheDocument();
+    expect(screen.getByText('GithubUserLogin')).toBeInTheDocument();
     userEvent.click(screen.getByText(/logout/i));
     expect(screen.getByText(/login to github/i)).toBeInTheDocument();
   });
