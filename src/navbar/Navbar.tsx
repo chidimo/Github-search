6566bb59180964cef0ce741a822dfd0b9bd32d4b
Chidi Orji <orjichidi95@gmail.com>
2021-11-11 04:32:15 +0100

    feat: hide form and header on search page

--- src/navbar/Navbar.tsx
+++ src/navbar/Navbar.tsx
@@ -1,13 +1,24 @@
+import { useLocation } from 'react-location';
+import clx from 'classnames';
 import { Branding } from '../shared/Branding';
 import { SearchForm } from '../shared/SearchForm';
 import styles from './navbar.module.scss';
 import { UserAvatar } from './UserAvatar';
 
 export const Navbar = (): JSX.Element => {
+  const { current } = useLocation();
   return (
-    <nav className={styles.navbar}>
-      <Branding hideNameOnSmall={true} addVerticalMargins={false} />
-      <SearchForm hasSubmitButton={false} />
+    <nav
+      className={clx([ styles.navbar ], {
+        [styles.flex_end]: current.pathname === '/',
+      })}
+    >
+      {current.pathname === '/results' && (
+        <>
+          <Branding hideNameOnSmall={true} addVerticalMargins={false} />
+          <SearchForm hasSubmitButton={false} />
+        </>
+      )}
       <UserAvatar />
     </nav>
   );
