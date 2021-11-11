1ea3172e87f7228ec9fa06c487000d711695fbd5
Chidi Orji <orjichidi95@gmail.com>
2021-11-11 04:20:16 +0100

    chore: render search pages in separate route

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ReactLocation, ReactLocationProvider, Routes } from 'react-location';
 import { useSelector } from 'react-redux';
 import { LoginWithGithub } from './login/LoginWithGithub';
 import { Navbar } from './navbar/Navbar';
@@ -6,18 +7,24 @@ import { SearchPage } from './search/SearchPage';
 import { selectLoginState } from './store/login';
 
 function App(): JSX.Element {
+  const reactLocation = new ReactLocation();
   const userLoggedIn = useSelector(selectLoginState);
+
   return (
-    <>
+    <ReactLocationProvider location={reactLocation}>
       {!userLoggedIn && <LoginWithGithub />}
       {userLoggedIn && (
         <>
           <Navbar />
-          {/* <SearchPage /> */}
-          <SearchResults />
+          <Routes
+            routes={[
+              { path: '/', element: <SearchPage /> },
+              { path: '/results', element: <SearchResults /> },
+            ]}
+          />
         </>
       )}
-    </>
+    </ReactLocationProvider>
   );
 }
 
