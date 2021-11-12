95b529553d9090524003ecb1a414f4144d7d1660
Chidi Orji <orjichidi95@gmail.com>
2021-11-12 04:51:27 +0100

    test: finish repo search test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,13 +1,17 @@
 import {
   render,
   screen,
+  waitFor,
   waitForElementToBeRemoved,
 } from './test-utils/render';
 import userEvent from '@testing-library/user-event';
 
 import App from './App';
+import { formatAsNumber } from './utils';
+import { repoSearchResults__page_1 } from './test-utils/repoSearchResults__page_1';
+import { repoSearchResults__page_2 } from './test-utils/repoSearchResults__page_2';
 
-describe('Authentication', () => {
+describe('Application', () => {
   window.open = jest.fn();
 
   it('Should login and logout user', async () => {
@@ -19,4 +23,50 @@ describe('Authentication', () => {
     userEvent.click(screen.getByText(/logout/i));
     expect(screen.getByText(/login to github/i)).toBeInTheDocument();
   });
+
+  it('Should initiate repo search from search page', async () => {
+    const SEARCH_TERM = 'react';
+    render(<App />);
+    userEvent.click(screen.getByText(/login to github/i));
+    await waitForElementToBeRemoved(() => screen.getByText(/Logging you in./i));
+    expect(screen.getByText(/search github/i)).toBeInTheDocument();
+    const searchInput = screen.getByPlaceholderText(/search/i);
+    await userEvent.type(searchInput, SEARCH_TERM, { delay: 100 });
+    expect(searchInput).toHaveValue(SEARCH_TERM);
+    userEvent.click(screen.getByText(/search github/i));
+
+    // this line stays further code execution until our route transition is complete
+    await waitFor(() => new Promise((res) => setTimeout(res, 0)));
+
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(/Loading repositories/i)
+    );
+
+    const repoCount = formatAsNumber(
+      repoSearchResults__page_1.search.repositoryCount
+    );
+    expect(
+      screen.getByText(`${repoCount} Repository results`)
+    ).toBeInTheDocument();
+    repoSearchResults__page_1.search.edges.forEach((edge: any) => {
+      expect(screen.getByText(edge.node.nameWithOwner)).toBeInTheDocument();
+    });
+    // expect previous pagination icon is not visible yet.
+    expect(screen.queryByTestId('paginationPrev')).not.toBeInTheDocument();
+
+    // go to next page
+    userEvent.click(screen.getByTestId('paginationNext'));
+    await waitForElementToBeRemoved(() =>
+      screen.getByText(/Loading repositories/i)
+    );
+
+    // expect previous pagination icon to now be visible
+    expect(screen.getByTestId('paginationPrev')).toBeInTheDocument();
+
+    repoSearchResults__page_2.search.edges.forEach((edge: any) => {
+      expect(screen.getByText(edge.node.nameWithOwner)).toBeInTheDocument();
+    });
+    // screen.debug();
+  });
 });
+
---
+++ src/test-utils/repoSearchResults__page_1.ts
@@ -0,0 +1,221 @@
+export const repoSearchResults__page_1 = {
+  search: {
+    __typename: 'SearchResultItemConnection',
+    repositoryCount: 2481262,
+    edges: [
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjE=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==',
+          url: 'https://github.com/facebook/react',
+          nameWithOwner: 'facebook/react',
+          updatedAt: '2021-11-12T01:35:31Z',
+          description:
+            'A declarative, efficient, and flexible JavaScript library for building user interfaces.',
+          stargazerCount: 177538,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjI=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnkxMzU3ODYwOTM=',
+          url: 'https://github.com/typescript-cheatsheets/react',
+          nameWithOwner: 'typescript-cheatsheets/react',
+          updatedAt: '2021-11-11T23:20:44Z',
+          description:
+            'Cheatsheets for experienced React developers getting started with TypeScript',
+          stargazerCount: 29692,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjM=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk3NTM5NjU3NQ==',
+          url: 'https://github.com/duxianwei520/react',
+          nameWithOwner: 'duxianwei520/react',
+          updatedAt: '2021-11-11T06:01:37Z',
+          description:
+            ' React+webpack+redux+ant design+axios+less全家桶后台管理框架',
+          stargazerCount: 4374,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjQ=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk5MDc1OTkzMA==',
+          url: 'https://github.com/discountry/react',
+          nameWithOwner: 'discountry/react',
+          updatedAt: '2021-11-11T03:24:43Z',
+          description: 'React docs in Chinese | React 中文文档翻译',
+          stargazerCount: 1044,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'Creative Commons Attribution 4.0 International',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjU=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk3NzUxMzQxOQ==',
+          url: 'https://github.com/react-redux-antd-es6/react',
+          nameWithOwner: 'react-redux-antd-es6/react',
+          updatedAt: '2021-11-11T10:11:22Z',
+          description: '基于react的企业后台管理开发框架',
+          stargazerCount: 877,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjY=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk3MjYyODI4NQ==',
+          url: 'https://github.com/Cathy0807/react',
+          nameWithOwner: 'Cathy0807/react',
+          updatedAt: '2021-11-08T10:55:10Z',
+          description: '京东首页构建',
+          stargazerCount: 736,
+          licenseInfo: null,
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjc=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnkyNDgyOTUxNzA=',
+          url: 'https://github.com/geist-org/react',
+          nameWithOwner: 'geist-org/react',
+          updatedAt: '2021-11-12T00:53:29Z',
+          description: 'Modern and minimalist React UI library.',
+          stargazerCount: 2473,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'TypeScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjg=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnkxMjE4MTQyMTA=',
+          url: 'https://github.com/primer/react',
+          nameWithOwner: 'primer/react',
+          updatedAt: '2021-11-11T20:17:52Z',
+          description:
+            "A React implementation of GitHub's Primer Design System",
+          stargazerCount: 1424,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'TypeScript',
+          },
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjk=',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk5MzUwMzU0NQ==',
+          url: 'https://github.com/HackYourFuture/React',
+          nameWithOwner: 'HackYourFuture/React',
+          updatedAt: '2021-11-10T14:29:49Z',
+          description:
+            'This repository contains all the material for the HackYourFuture module "React.js: Building dynamic UIs with modern JavaScript"',
+          stargazerCount: 156,
+          licenseInfo: null,
+          primaryLanguage: null,
+        },
+      },
+      {
+        __typename: 'SearchResultItemEdge',
+        cursor: 'Y3Vyc29yOjEw',
+        node: {
+          __typename: 'Repository',
+          id: 'MDEwOlJlcG9zaXRvcnk3MDEwNzc4Ng==',
+          url: 'https://github.com/vercel/next.js',
+          nameWithOwner: 'vercel/next.js',
+          updatedAt: '2021-11-12T01:25:42Z',
+          description: 'The React Framework',
+          stargazerCount: 76415,
+          licenseInfo: {
+            __typename: 'License',
+            name: 'MIT License',
+          },
+          primaryLanguage: {
+            __typename: 'Language',
+            name: 'JavaScript',
+          },
+        },
+      },
+    ],
+    pageInfo: {
+      __typename: 'PageInfo',
+      startCursor: 'Y3Vyc29yOjE=',
+      endCursor: 'Y3Vyc29yOjEw',
+      hasNextPage: true,
+      hasPreviousPage: false,
+    },
+  },
+};
