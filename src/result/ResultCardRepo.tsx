fa06b8e31516681ca41299341915b8a6756503d8
Chidi Orji <orjichidi95@gmail.com>
2021-11-14 02:14:31 +0100

    bug: fix repo meta details

--- src/result/ResultCardRepo.tsx
+++ src/result/ResultCardRepo.tsx
@@ -1,6 +1,7 @@
 import { formatNumberWithk } from '../utils';
 import { RepoResultInterface } from './interfaces';
 import styles from './ResultCardRepo.module.scss';
+import { parseISO, formatDistanceToNow } from 'date-fns';
 
 type ResultCardProps = {
   result: RepoResultInterface;
@@ -17,16 +18,21 @@ export const ResultCardRepo = (props: ResultCardProps): JSX.Element => {
     primaryLanguage,
     updatedAt,
   } = result;
+  const date = updatedAt ? new Date(updatedAt) : new Date();
+
+  const about = [
+    `${formatNumberWithk(stargazerCount)} Stars`,
+    primaryLanguage ? `${primaryLanguage.name}` : '',
+    licenseInfo ? `${licenseInfo.name}` : '',
+    `Updated ${formatDistanceToNow(parseISO(date.toISOString()))} ago`,
+  ];
   return (
     <div className={styles.result_card_container}>
-      <h1 className='card_title__link'>
+      <h1 className="card_title__link">
         <a href={url}>{nameWithOwner}</a>
       </h1>
       <p>{description}</p>
-      <p>
-        {formatNumberWithk(stargazerCount)} Stars | {primaryLanguage?.name} |{' '}
-        {licenseInfo?.name} | {updatedAt}
-      </p>
+      <p>{about.filter((a) => a !== '').join(' | ')}</p>
     </div>
   );
 };
